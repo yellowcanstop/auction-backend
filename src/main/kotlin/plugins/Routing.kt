057baf91b754612bd7324ad94d6db6da11dbf71d
Wong Huey Teng <yellowcanstop@gmail.com>
2025-11-18 15:49:22 +0800

    Add / for health check

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -4,11 +4,16 @@ import com.example.routes.auctionRoutes
 import com.example.routes.authRoutes
 import com.example.routes.groupRoutes
 import com.example.routes.taskRoutes
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        get("/") {
+            call.respond(HttpStatusCode.OK)
+        }
         authRoutes()
         groupRoutes()
         taskRoutes()
